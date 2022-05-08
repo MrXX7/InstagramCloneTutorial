5cfac5b0fe0966a79f967bae4304aadd41b4b53b
Oncu <oohancan@gmail.com>
2022-05-08 11:29:13 +0300

    Initial Commit

---
+++ InstagramCloneTutorial/InstagramCloneTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneTutorialApp.swift
+//  InstagramCloneTutorial
+//
+//  Created by Oncu Ohancan on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
