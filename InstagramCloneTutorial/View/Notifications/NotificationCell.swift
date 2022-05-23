74053277d493924395348bd47991825b53812edb
Oncu <oohancan@gmail.com>
2022-05-23 06:11:50 +0300

    add and edit notificationCell

--- InstagramCloneTutorial/View/Notifications/NotificationCell.swift
+++ InstagramCloneTutorial/View/Notifications/NotificationCell.swift
@@ -13,37 +13,53 @@ struct NotificationCell: View {
     let notification: Notification
     var body: some View {
         HStack {
-            if let imageURL = notification.profileImageURL {
-                KFImage(URL(string: imageURL))
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .clipShape(Circle())
+            if let user = notification.user {
+                NavigationLink(destination: ProfileView(user: user)) {
+                    if let user = notification.user {
+                        if let imageURL = notification.profileImageURL {
+                            KFImage(URL(string: imageURL))
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                        }
+                        
+                        Text(notification.username)
+                            .font(.system(size: 14, weight: .semibold))
+                        +
+                        Text(notification.type.notificationsMessage)
+                            .font(.system(size: 15))
+                        +
+                        Text(" 2H")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 12))
+                }
             }
             
-            Text(notification.username)
-                .font(.system(size: 14, weight: .semibold))
-            +
-            Text(" has followed you.")
-                .font(.system(size: 15))
-            +
-            Text(" 2H")
-                .foregroundColor(.gray)
-                .font(.system(size: 12))
-            
             Spacer()
-            
-            Text("Follow")
-                .font(.system(size: 14, weight: .semibold))
-                .frame(width: 100, height: 32)
-                .foregroundColor(.white)
-                .background(.blue)
-                .cornerRadius(3)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 3)
-                        .stroke(Color.gray, lineWidth: 1)
-            )
-        }
-        .padding(.horizontal)
+                
+                if notification.type == .follow {
+                    Text("Follow")
+                        .font(.system(size: 14, weight: .semibold))
+                        .frame(width: 100, height: 32)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(3)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 3)
+                                .stroke(Color.gray, lineWidth: 1)
+                    )
+                }
+                else {
+                    if let post = notification.post {
+                        KFImage(URL(string: post.imageURL))
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 40)
+                            .clipped()
+                    }
+                }
+            }
+        }.padding(.horizontal)
     }
 }
