21d37e3ea00c2320269d7e34d22b92e9ac7c891c
Oncu <oohancan@gmail.com>
2022-05-30 21:41:50 +0300

    edit message and messagechatview

--- InstagramCloneTutorial/View/Messages/MessageChatView.swift
+++ InstagramCloneTutorial/View/Messages/MessageChatView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MessageChatView: View {
     @State var message = ""
     @ObservedObject var viewModel: MessagesViewModel
+    @State var scrolled = false
     
     init(userId: String) {
         self.viewModel = MessagesViewModel(userId: userId)
@@ -17,11 +18,20 @@ struct MessageChatView: View {
     
     var body: some View {
         VStack {
-            ScrollViewReader { scroll in
+            ScrollViewReader { reader in
             ScrollView {
                 LazyVStack {
                     ForEach(viewModel.messages) { message in
                     MessageRowView(message: message)
+                            .onAppear{
+                                if message.id == viewModel.messages.last?.id && !scrolled {
+                                    reader.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
+                                    scrolled = true
+                                }
+                            }
+                    }
+                    .onChange(of: viewModel.messages) { _ in
+                        reader.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
                     }
                 }
             }
